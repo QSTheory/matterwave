86ff1b8fcfc08a38967d3b9d8500fe5123d7e968
Gabriel Müller <g.mueller@iqo.uni-hannover.de>
2024-02-02 15:07:07 +0100

    Update FFTDimension usage in test_panel_plot.py

--- matterwave/helpers/test_panel_plot.py
+++ matterwave/helpers/test_panel_plot.py
@@ -1,12 +1,11 @@
 
-
-from fftarray import FFTDimension
+from fftarray.fft_constraint_solver import fft_dim_from_constraints
 
 from matterwave.helpers import plot_fftarray
 
-x_dim = FFTDimension(name="x", n=64, pos_middle=0, freq_middle=0, d_pos=1)
-y_dim = FFTDimension(name="y", n=64, pos_middle=0, freq_middle=0, d_pos=1)
-z_dim = FFTDimension(name="z", n=64, pos_middle=0, freq_middle=0, d_pos=1)
+x_dim = fft_dim_from_constraints(name="x", n=64, pos_middle=0, freq_middle=0, d_pos=1)
+y_dim = fft_dim_from_constraints(name="y", n=64, pos_middle=0, freq_middle=0, d_pos=1)
+z_dim = fft_dim_from_constraints(name="z", n=64, pos_middle=0, freq_middle=0, d_pos=1)
 
 one_dim_fftarray = x_dim.pos_array()
 two_dim_fftarray = x_dim.pos_array() + y_dim.pos_array()

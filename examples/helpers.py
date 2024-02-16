b96bbe0cb3b773d414a612b6a3c45f0aec63aa0b
Christian Struckmann <christian.struckmann@gmx.de>
2024-02-16 12:52:26 +0100

    Update plotting function to abs squared

--- examples/helpers.py
+++ examples/helpers.py
@@ -5,22 +5,20 @@ from bokeh.plotting import figure, row, show
 
 from fftarray import FFTArray
 
-def plt_fftarray(
+def plt_wavefunction(
         arr: FFTArray,
         data_name: Optional[str] = None,
         show_plot: bool = True,
     ):
     if len(arr.dims) == 1:
         dim = arr.dims[0]
-        p_pos = figure(width=450, height=400, x_axis_label = f"{dim.name} pos coordinate", min_border=50)
-        p_pos.line(dim.np_array(space="pos"), np.real(arr.into(space="pos").values), line_width=2, color = "navy", legend_label="real")
-        p_pos.line(dim.np_array(space="pos"), np.imag(arr.into(space="pos").values), line_width=2, color = "firebrick", legend_label="imag")
-        p_pos.title.text = f"{data_name or 'FFTArray values'} shown in position space" # type: ignore
+        p_pos = figure(width=450, height=400, x_axis_label = f"{dim.name} pos coordinate [m]", min_border=50)
+        p_pos.line(dim.np_array(space="pos"), np.abs(arr.into(space="pos").values)**2, line_width=2)
+        p_pos.title.text = f"{data_name or 'Wavefunction'} propability in position space" # type: ignore
 
-        p_freq = figure(width=450, height=400, x_axis_label = f"{dim.name} freq coordinate", min_border=50)
-        p_freq.line(dim.np_array(space="freq"), np.real(arr.into(space="freq").values), line_width=2, color = "navy", legend_label="real")
-        p_freq.line(dim.np_array(space="freq"), np.imag(arr.into(space="freq").values), line_width=2, color = "firebrick", legend_label="imag")
-        p_freq.title.text = f"{data_name or 'FFTArray values'} shown in frequency space" # type: ignore
+        p_freq = figure(width=450, height=400, x_axis_label = f"{dim.name} freq coordinate [1/m]", min_border=50)
+        p_freq.line(dim.np_array(space="freq"), np.abs(arr.into(space="freq").values)**2, line_width=2)
+        p_freq.title.text = f"{data_name or 'Wavefunction'} probability in frequency space" # type: ignore
 
         plot = row([p_pos, p_freq], sizing_mode="stretch_width") # type: ignore
     else:
